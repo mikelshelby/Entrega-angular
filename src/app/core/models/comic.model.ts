d93ac16e6bc6deef7eeb715f31f2425c60a88b40
Jaime <jaime.navarro@gmail.com>
2022-06-10 17:43:19 +0200

    Añade fecha de venta al modelo Comic

--- src/app/core/models/comic.model.ts
+++ src/app/core/models/comic.model.ts
@@ -1,6 +1,6 @@
-import { Image } from "@core/interfaces/image.interface";
-import { Model } from "@core/interfaces/model.interface";
-import { Creator } from "@core/models/creator.model";
+import { Image } from '@core/interfaces/image.interface';
+import { Model } from '@core/interfaces/model.interface';
+import { Creator } from '@core/models/creator.model';
 
 export class Comic implements Model {
   private _id!: number;
@@ -10,66 +10,82 @@ export class Comic implements Model {
   private _pageCount!: number;
   private _images!: Image[];
   private _creators!: Creator[];
+  private _onsaleDate!: Date;
 
   public get id(): number {
-      return this._id;
+    return this._id;
   }
   public set id(value: number) {
-      this._id = value;
+    this._id = value;
   }
   public get title(): string {
-      return this._title;
+    return this._title;
   }
   public set title(value: string) {
-      this._title = value;
+    this._title = value;
   }
   public get description(): string {
-      return this._description;
+    return this._description;
   }
   public set description(value: string) {
-      this._description = value;
+    this._description = value;
   }
   public get isbn(): string {
-      return this._isbn;
+    return this._isbn;
   }
   public set isbn(value: string) {
-      this._isbn = value;
+    this._isbn = value;
   }
   public get pageCount(): number {
-      return this._pageCount;
+    return this._pageCount;
   }
   public set pageCount(value: number) {
-      this._pageCount = value;
+    this._pageCount = value;
   }
   public get images(): Image[] {
-      return this._images;
+    return this._images;
   }
   public set images(value: Image[]) {
-      this._images = value;
+    this._images = value;
   }
   public get creators(): Creator[] {
-      return this._creators;
+    return this._creators;
   }
   public set creators(value: Creator[]) {
-      this._creators = value;
+    this._creators = value;
+  }
+  public get onsaleDate(): Date {
+    return this._onsaleDate;
+  }
+  public set onsaleDate(value: Date) {
+    this._onsaleDate = value;
   }
 
   constructor(input: any) {
-      Object.assign(this, input);
+    Object.assign(this, input);
 
-      // if (input.creators && input.creators.items) {
-      //     this.creators = input.creators.items.map((c: any) => new Creator(c));
-      // }
+    if (input && input.dates && input.dates.length > 0) {
+      const filterDates = input.dates.filter(
+        (d: any) => d.type === 'onsaleDate'
+      );
+      if (filterDates && filterDates.length > 0) {
+        this.onsaleDate = new Date(filterDates.date);
+      }
+    }
+
+    if (input.creators && input.creators.items) {
+      this.creators = (input.creators.items as any[]).map((c: any) => new Creator(c));
+    }
   }
   serialize() {
-      return {
-          id: this.id,
-          title: this.title,
-          description: this.description,
-          isbn: this.isbn,
-          pageCount: this.pageCount,
-          images: this.images,
-          creators: this.creators.map(c => c.serialize())
-      }
+    return {
+      id: this.id,
+      title: this.title,
+      description: this.description,
+      isbn: this.isbn,
+      pageCount: this.pageCount,
+      images: this.images,
+      creators: this.creators.map((c) => c.serialize()),
+    };
   }
 }
