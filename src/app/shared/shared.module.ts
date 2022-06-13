b3f821e31a1b34ca5e6f255f45400380091adbb6
Jaime <jaime.navarro@gmail.com>
2022-06-13 12:10:02 +0200

    Pipes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './components/loader/loader.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
+import { CreatorsPipe } from './pipes/creators.pipe';
 
 
 
 @NgModule({
   declarations: [
     LoaderComponent,
-    PaginationComponent
+    PaginationComponent,
+    CreatorsPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
     LoaderComponent,
-    PaginationComponent
+    PaginationComponent,
+    CreatorsPipe
   ]
 })
 export class SharedModule { }
